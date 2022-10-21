7cd0c70e3b4c596b37e7e74c45528812ffc950aa
alexanderkulia <kaaprivatepropert@gmail.com>
2022-10-21 16:15:55 +0200

    init

---
+++ basics.ts
@@ -0,0 +1,37 @@
+/*
+
+MOVEMENT
+h - left
+j - down
+k - up
+l - right
+
+Enable key repeat for VS Code
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+For Pycharm
+defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
+
+Globally in MacOS
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+FASTER MOVEMENT
+w (word) - jump forwards by a word
+b (back) - jump backwards by a word
+
+MODES
+i (insert) - insert before cursor
+a (after) - insert after cursor
+v (visual) - enter visual mode
+V - enter visual line mode
+<esc> or ctrl+c - return to command mode
+
+COPY PASTE
+y (yank) - copy
+d (delete) - delete
+p (paste) - paste from register
+
+HELP I'M STUCK IN VIM
+:q!
+
+*/
---
+++ index.ts
@@ -0,0 +1,39 @@
+// TASK: Rename num argument to somenum
+// OPTION 1: navigate with hjkl, dw i somenum ctrl+c
+const addOne = (num: number): number => {
+  return num + 1;
+};
+
+// OPTION 2: wwww dw somenum ctrl+c
+const addOne = (num: number): number => {
+  return num + 1;
+};
+
+// OPTION 3: 4w dw somenum ctrl+c
+// OPTION 3.1: 4w cw somenum cltr+c
+// c (change) - deletes and enters insert mode
+const addOne = (num: number): number => {
+  return num + 1;
+};
+
+// TASK: Delete prop_5, prop_6 and prop_7
+// OPTION 1: V 2j d
+// OPTION 2: dd dd dd
+// OPTION 3: 3dd
+const mapObject = (obj) => {
+  return {
+    prop_1: obj.prop1,
+    prop_2: obj.prop2,
+    prop_3: obj.prop3,
+    prop_4: obj.prop4,
+    prop_5: obj.prop5,
+    prop_6: obj.prop6,
+    prop_7: obj.prop7,
+    prop_8: obj.prop8,
+    prop_9: obj.prop9,
+    prop_10: obj.prop10,
+    prop_11: obj.prop11,
+    prop_12: obj.prop12,
+    prop_13: obj.prop13,
+  };
+};
