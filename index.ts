57916c5b8f666fb1193d9e1147c083e0c07e13f1
alexanderkulia <37775912+AlexanderKulia@users.noreply.github.com>
2022-10-21 19:30:32 +0200

    adds some more tasks

--- index.ts
+++ index.ts
@@ -16,6 +16,17 @@ const addOne = (num: number): number => {
   return num + 1;
 };
 
+// TASK: Replace {} with []
+// OPTION 1: using w
+// OPTION 2: f{ xx i []
+const someVeryLongString =
+  "This is a very long string has has a {} in it for some reason";
+
+// TASK: Rename element to newelement
+// OPTION 1: 4w ciw newelement
+// OPTION 2: ci" anywhere on the line
+const someArray = ["element"];
+
 // TASK: Delete prop_5, prop_6 and prop_7
 // OPTION 1: V 2j d
 // OPTION 2: dd dd dd
@@ -37,3 +48,37 @@ const mapObject = (obj) => {
     prop_13: obj.prop13,
   };
 };
+
+// TASK: Remove contents of the function
+// OPTION 1: S to delete line and enter insert mode
+// OPTION 2: di{
+function badFunction() {
+  // some bad code
+}
+
+// TASK: Extract object into a variable called data
+// OPTION 1: jump to line, f{ v% d
+// OPTION 2: anywhere inside the object, va{ d
+console.log({
+  a: 1,
+  b: 2,
+  c: 3,
+});
+
+// TASK: Replace object to { d: 4, e: 5, f: 6 }
+// OPTION 1: Y new object, 2j
+// OPTION 2: vi{ d O
+console.log({
+  a: 1,
+  b: 2,
+  c: 3,
+});
+
+// TASK: Put items into array as strings
+// OPTION 1: V 4j :s/(\w.*)/data[0] = "\1"
+const data: string[] = [];
+// one
+// two
+// three
+// four
+// five
